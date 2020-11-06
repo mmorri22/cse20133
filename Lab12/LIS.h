ed579ba6abe4972a6d90208c64e8565147bf7692
Matthew Morrison <matt.morrison@nd.edu>
2020-11-06 12:25:18 -0500

    Lab 12 Fix

--- Lab12/LIS.h
+++ Lab12/LIS.h
@@ -6,15 +6,23 @@
 #include <vector> /* Calls the vector library */
 
 template<class T>
-void FindAndUpdateElement( std::vector< T >& LIS_Vec, T searchVal  ){
+void FindAndUpdateElement( std::vector< T >& LIS_Vec, T searchVal  );
 
+template<class T>
+long unsigned int getLIS( const std::vector< T >& theVec );
 
-
-}
 
 template<class T>
 long unsigned int getLIS( const std::vector< T >& theVec ){
 
+	// Write Code Here
+}
+
+
+template<class T>
+void FindAndUpdateElement( std::vector< T >& LIS_Vec, T searchVal  ){
+
+	// Write Code Here
 
 }
 
