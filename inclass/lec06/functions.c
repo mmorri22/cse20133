2a58c03da574198b55566a569194ed2e3cd53436
Matthew Morrison <mmorri22@nd.edu>
2023-08-19 13:49:20 -0400

    Lecture 06 Part 2 Push

---
+++ inclass/lec06/functions.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* Function Declarations */
+
+int main( void ){
+
+	/* Create Variables */
+	/* Note: Only create variables we are using at intermediate steps */
+
+	/* Average of two doubles */
+
+	/* Call if-else function */
+
+	/* Call while function */
+
+	/* Call for function */
+
+	return 0;
+}
+
+/* Function Definitions */
