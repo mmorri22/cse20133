9dca9af54495d4ce15db616abad31d50efe5e580
Matthew Morrison <mmorri22@nd.edu>
2023-08-19 19:02:36 -0400

    Lecture 09 In Class Initial Push

---
+++ inclass/lec09/setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/fib.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/fib_memo.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/trace.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/trace_ptr.c
+rm setup.sh
