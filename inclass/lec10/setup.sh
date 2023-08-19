6ee67548585f08faad17409bb48ef8aba8f58d61
Matthew Morrison <mmorri22@nd.edu>
2023-08-19 19:41:52 -0400

    Lecture 10 In Class

--- inclass/lec10/setup.sh
+++ inclass/lec10/setup.sh
@@ -1,6 +1,8 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/fib.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/fib_memo.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/trace.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/trace_ptr.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/fib.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/fib_memo.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/maze.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/maze_demo.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/trace.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/trace_ptr.c
 rm setup.sh
