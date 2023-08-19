abdc915d7dd8a043326647aebbc7537ada295004
Matthew Morrison <mmorri22@nd.edu>
2023-08-19 17:48:08 -0400

    Lecture 08 Update

---
+++ inclass/lec08/hours.c
@@ -0,0 +1,45 @@
+/**************************************
+ * File name: insertion_sort.c
+ * Initial Author: Prof. Matthew Morrison
+ * Modified: [Your Name],[Date]
+ * Email: [Your Email]
+ *
+ * This function contains the main driver program
+ * For the insertion sort in-class coding opportunity
+ ***************************************/
+
+#include <stdio.h>
+
+/* Step 6 - Include util.h library here when we are doing the Header Files */
+
+/* Step 1 - Compiler Directive MINUTES */
+
+/* Step 3 - Function prototypes */
+
+
+int main()
+{
+    /* Declared Variables */
+    unsigned int time, hours, minutes;
+
+    fprintf( stdout, "Test program to convert time values\n" );
+    fprintf( stdout, "Enter a time duration in minutes: " );
+
+    /* Step 2 - First Attempt at fscanf */
+
+
+    /* Step 2b - Print the result to the terminal */
+
+
+    /* Step 5 - Call the convert_time_to_hm function with hours and minutes passed by ref */
+
+
+    /* Print the result */
+    fprintf(stdout, "HH:MM format: %u:%02u\n", hours, minutes);
+
+    return 0;
+}
+
+/* Function Definitions */
+
+/* Step 4 - Write the function definition of convert_time_to_hm */
---
+++ inclass/lec08/setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/hours.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/ptr_trace.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/fscanf.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/insert_sort.c
+rm setup.sh
