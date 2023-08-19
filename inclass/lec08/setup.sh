1ce8c0450095394432bf42d812fe6a12eb680499
Matthew Morrison <mmorri22@nd.edu>
2023-08-19 18:16:11 -0400

    Lecture 08 Update

--- inclass/lec08/setup.sh
+++ inclass/lec08/setup.sh
@@ -1,6 +1,5 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/hours.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/ptr_trace.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/fscanf.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec07/insert_sort.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/hours.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/ptr_trace.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/fscanf.c
 rm setup.sh
