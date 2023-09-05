d68a07e2c8c13f2d759211f1f057c2499cc826f2
Matthew Morrison <mmorri22@nd.edu>
2023-09-05 10:41:21 -0400

    In-Class 4 and 5 Sols

---
+++ inclass-sols/lec04/hello.c
@@ -0,0 +1,13 @@
+/* Name: Matthew Morrison */
+/* Email: mmorri22@nd.edu */
+/* Filename: hello.c */
+
+#include <stdio.h>
+
+int main(void){
+
+    fprintf( stdout, "Hello, World %p\n", stdout );
+
+    // Return to the OS
+    return 0;
+}
---
+++ inclass-sols/lec04/void_return.c
@@ -0,0 +1,9 @@
+/* Name: Matthew Morrison */
+/* Email: mmorri22@nd.edu */
+/* Filename: void_return.c */
+
+int main(void){
+
+    // Return to the OS
+    return 0;
+}
