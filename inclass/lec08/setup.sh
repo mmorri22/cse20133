586d12ebb3f67a4a9190821925581cd3869436a4
Matthew Morrison <mmorri22@nd.edu>
2023-09-08 11:39:54 -0400

    Update Lecture 08

---
+++ inclass/lec08/setup.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/parallel.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/parallel_lib.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/parallel_lib.h
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/parallel_run.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec08/fact_double_sci.c
+rm setup.sh
