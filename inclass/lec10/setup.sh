8e8d2d0620d0f0d9c664b24fcadcd280733ee691
Matthew Morrison <mmorri22@nd.edu>
2023-09-10 18:28:22 -0400

    Lecture 10 In-Class Final

--- inclass/lec10/setup.sh
+++ inclass/lec10/setup.sh
@@ -1,3 +1,4 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/static_basic.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/insertion_sort.c
 rm setup.sh
