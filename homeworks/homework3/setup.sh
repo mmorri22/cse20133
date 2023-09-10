b491adb6287591578ae9090033f92a0b0fde79a1
Matthew Morrison <mmorri22@nd.edu>
2023-09-09 21:10:25 -0400

    Homework 3 Final Update

--- homeworks/homework3/setup.sh
+++
@@ -1,5 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework3/Prob1.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework3/Prob1_mod.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework3/Prob2_1.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework3/Prob2_2.c
-rm setup.sh
