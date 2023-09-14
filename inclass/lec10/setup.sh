66f7a41212ae708946ce6fef8fd08e933e34c3fa
Matthew Morrison <mmorri22@nd.edu>
2023-09-14 14:14:06 -0400

    Lecture 10 In Class Update

--- inclass/lec10/setup.sh
+++ inclass/lec10/setup.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/static_basic.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/insertion_sort.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/fscanf.c
 rm setup.sh
--- inclass/lec10/static_pbr.c
+++ inclass/lec10/static_pbr.c
@@ -7,10 +7,16 @@
 int main(){
 
   /* Allocate the static integer values */
-
+  int int_array[] = {22, 17, -15, 8, 2, 15};
+  long unsigned int array_len = 6;
 
   /* Iterate through the array and print the values */
+  long unsigned int iter;
+  for( iter = 0; iter < array_len; ++iter ){
 
+    fprintf( stdout, "int_array[%lu] = %d at %p\n", iter, int_array[iter], &int_array[iter] );
+
+  }
 
   return 0;
 }
