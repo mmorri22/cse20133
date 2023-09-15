21163d5e31fd2db510b9e6c2bb32e31a521ba52f
Matthew Morrison <mmorri22@nd.edu>
2023-09-14 21:18:06 -0400

    Lecture 11 In Class Update

--- inclass/lec11/fib_memo.c
+++ inclass/lec11/fib_memo.c
@@ -13,7 +13,7 @@ int main( void ){
 
 	/* Step 1 - Initialize a static array of fib_num size */
 	/* Step 7 - We have a modification to make here... */
-
+	int fib_array[FIB_NUM] = { [0 ... (FIB_NUM-1)] = 0 };
 
 	fprintf( stdout, "The %d Fib Number is = %d\n", FIB_NUM, Fibonacci( FIB_NUM - 1, fib_array ) );
 
@@ -27,16 +27,22 @@ int main( void ){
 int Fibonacci( int fib_num, int fib_array[] ){
 
 	/* Step 3 - Check the array value */
-
+	if( fib_array[ fib_num ] != 0 ){
+		return fib_array[ fib_num ];
+	}
 
 	/* Step 4 - Set the array value to 1 if fib_num is 0 or 1 */
-
+	if( fib_num == 0 || fib_num == 1){
+		fib_array[ fib_num ] = 1;
+	}
 
 	/* Step 5 - Update the array with the binary conversion */
-
+	else{
+		fib_array[ fib_num ] = Fibonacci( fib_num - 1, fib_array ) + Fibonacci( fib_num - 2, fib_array );
+	}
 
 	/* Step 6 - Return the array value */
-
+	return fib_array[ fib_num ];
 
 }
 
