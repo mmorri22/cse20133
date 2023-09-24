4f26cf48d18d8611ca3691b3067d5fb12c0f7f68
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 12:41:05 -0400

    Lecture 16 Reading Update

--- inclass/lec16/setup.sh
+++ inclass/lec16/setup.sh
@@ -1,8 +1,8 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/better_malloc.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/better_malloc_li.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/better_malloc_float.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/better_malloc_double.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/argc_argv.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/argc_argv_mod.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec16/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec16/better_malloc.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec16/better_malloc_li.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec16/better_malloc_float.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec16/better_malloc_double.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec16/argc_argv.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec16/argc_argv_mod.c
 rm setup.sh
