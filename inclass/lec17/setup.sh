3102e816976a49980680ce8b5e2246d8e764683d
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 13:07:54 -0400

    Lecture 17 In-Class

---
+++ inclass/lec17/setup.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec17/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec17/maze_demo.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec17/maze.c
+rm setup.sh
