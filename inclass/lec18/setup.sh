16263d0a8395b53ac1a573e18f83c606c4e8fe26
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 13:24:58 -0400

    Lecture 18 In-Class Initial Update

---
+++ inclass/lec18/setup.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/structs.c
+rm setup.sh
