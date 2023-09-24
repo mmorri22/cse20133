b9e6e87f769fc65cc12da8c10c8555fb8b33bf9e
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 19:35:15 -0400

    Lecture 22 Initial Push

--- inclass/lec18/setup.sh
+++ inclass/lec18/setup.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/structs.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec17/mem_map.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/mem_map.c
 rm setup.sh
---
+++ inclass/lec22/setup.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec22/gdb-part1.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec22/gdb-part2.c
+rm setup.sh
