78f89d892162d5b7b4a3a0c7de3020178a6f5b9b
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 11:29:06 -0400

    Lecture 14 In Class Final Fix

--- inclass/lec14/setup.sh
+++ inclass/lec14/setup.sh
@@ -1,4 +1,3 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec14/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec14/stat_dyn_addr.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec14/dyn_array_time.c
 rm setup.sh
