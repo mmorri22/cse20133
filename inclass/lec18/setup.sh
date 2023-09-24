128055d945fecd080a4687fdc2ee0c9d70b7c56e
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 15:17:08 -0400

    Lecture 17 In Class Fix

--- inclass/lec18/setup.sh
+++ inclass/lec18/setup.sh
@@ -1,3 +1,4 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/structs.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec17/mem_map.c
 rm setup.sh
