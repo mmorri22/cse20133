60f8beb093ac08888ca602ef58a107ef29d326e1
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 17:52:26 -0400

    Lecture 19 Reading Update

--- readings/lec19/file_locations.c
+++ readings/lec19/file_locations.c
@@ -3,8 +3,26 @@
 
 int main( const int argc, const char* argv[] ){
 
-  fprintf( stdout, "stdout on the stack at %p\n", stdout );
-  fprintf( stdout, "stdin on the stack at %p\n", stdin );
+  FILE* print_out =  fopen("example_output.txt", "w");
+
+  if( print_out == NULL ){
+      exit(-1);
+  }
+
+  fprintf( print_out, "stdout on the stack at %p\n", stdout );
+  fprintf( print_out, "stdin  on the stack at %p\n", stdin );
+  fprintf( print_out, "print_out  on the heap at %p\n", print_out );
+
+  fprintf( print_out, "Address and value of argc: %p %d\n", &argc, argc );
+  fprintf( print_out, "Register and Base Address of argv: %p %p\n", &argv, argv );
+
+  int iter;
+  for( iter = 0; iter < argc; ++iter ){
+      fprintf( stdout, "Printing to stdout: %s %p\n", argv[iter], &argv[iter] );
+      fprintf( print_out, "Printing to print_out: %s %p\n", argv[iter], &argv[iter] );
+  }
+
+  fclose(print_out);
 
   return EXIT_SUCCESS;
 }
