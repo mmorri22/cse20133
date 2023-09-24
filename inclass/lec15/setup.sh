7b1d146c03d7b83c0196acad5828c2083582d731
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 12:13:37 -0400

    Lecture 15 In-Class

--- inclass/lec15/setup.sh
+++ inclass/lec15/setup.sh
@@ -1,11 +1,4 @@
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/Makefile
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/stat_dyn_addr.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/dyn_array_time.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/void_recast.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/line1.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/line2.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/line3.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/line1_ex.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/line2_ex.c
-wget https://raw.githubusercontent.com/mmorri22/su23-cse20332/main/inclass/lec07/line3_ex.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/stat_dyn_func.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/dyn_array_time.c
 rm setup.sh
