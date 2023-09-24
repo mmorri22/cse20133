13f96647f731f6b53f357d7f9400dd8a30bc4d7f
Matthew Morrison <mmorri22@nd.edu>
2023-09-24 11:04:03 -0400

    Lecture 14 Update

--- inclass/lec14/setup.sh
+++ inclass/lec14/setup.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/void_recast.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec13/stat_dyn_addr.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec14/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec14/stat_dyn_addr.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec14/dyn_array_time.c
 rm setup.sh
