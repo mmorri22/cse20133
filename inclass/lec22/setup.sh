ceaeda790e21205d68c9f78cabe165a5877d1d76
Matthew Morrison <mmorri22@nd.edu>
2023-10-09 02:19:45 -0400

    Lecture 22 In-Class Update

---
+++ inclass/lec22/setup.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec22/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec22/lec22.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec22/lec22_sol.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec22/library.txt
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec22/struct_align.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec22/struct_align_fix.c
+rm setup.sh
