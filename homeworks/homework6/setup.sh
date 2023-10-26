cf9d85af52f0b34aad76c2b2b676ba13d9a12fb6
Matthew Morrison <mmorri22@nd.edu>
2023-10-26 11:28:32 -0400

    Lecture 06 Setup Script

--- homeworks/homework6/setup.sh
+++ homeworks/homework6/setup.sh
@@ -1,6 +1,6 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework6/homework06.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework6/maze_generate.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework6/maze_generator.c
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework6/labyrinth.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework6/maze_generate.h
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework6/labyrinth.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework6/maze_generator.h
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework6/labyrinth.h
 rm setup.sh
