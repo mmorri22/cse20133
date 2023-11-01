5b4b68449ad56c35b28d694590b63cb2159658e8
Matthew Morrison <mmorri22@nd.edu>
2023-11-01 09:12:00 -0400

    Lecture 27 In-Class Fix

--- inclass/lec27/setup.sh
+++ inclass/lec27/setup.sh
@@ -4,4 +4,5 @@ wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/coun
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/count_sort_sol.cpp
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/hanoi_stl.cpp
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/two_sums.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/sort_comp.cpp
 rm setup.sh
