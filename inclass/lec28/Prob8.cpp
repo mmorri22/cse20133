0d2ecb9f07966e6c0f516226f86dd6d245500fe3
Matthew Morrison <mmorri22@nd.edu>
2023-11-06 09:03:50 -0500

    Lecture 28 Final Update

--- inclass/lec28/Prob8.cpp
+++ inclass/lec28/Prob8.cpp
@@ -10,28 +10,28 @@ void func( std::vector<int>& the_vec ){
     std::queue<int> the_queue;
 
     for( long unsigned int iter = 0; iter < the_vec.size(); ++iter ){
-        
+
         if(the_vec.at(iter) % 2 == 0 )
             the_stack.push( the_vec.at(iter) );
-            
+
         else
             the_queue.push( the_vec.at(iter) );
-        
+
     }
 
     while( !the_stack.empty() ){
-        
+
         std::cout << the_stack.top() << " ";
-        
+
         the_stack.pop();
     }
 
     while( !the_queue.empty() ){
-        
+
         std::cout << the_queue.front() << " ";
-        
+
         the_queue.pop();
-    }   
+    }
 
     std::cout << std::endl;
 
@@ -40,8 +40,6 @@ void func( std::vector<int>& the_vec ){
 int main()
 {
     std::vector<int> the_vec = {22, 7, 14, 45, 8, 81, 24, 27};
-
-    std::sort( the_vec.begin(), the_vec.end() );
 
     func( the_vec );
 
