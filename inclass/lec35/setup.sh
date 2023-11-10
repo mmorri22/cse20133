eb76a059fb798b79b1b3c6fdbda4f6118f5d7618
Matthew Morrison <mmorri22@nd.edu>
2023-11-10 11:58:27 -0500

    Lecture 33 In Class Update

--- inclass/lec35/setup.sh
+++
@@ -1,8 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec35/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec35/pthread_1.cpp
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec35/pthread_2.cpp
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec35/pthread_3.cpp
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec35/pthread_in_class.cpp
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec35/pthread_in_class_rc.cpp
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec35/pthread_in_class_sol.cpp
-rm setup.sh
