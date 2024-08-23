8d23b5bbfcc66f4653e177622a514c837a3c96ee
Matthew Morrison <mmorri22@nd.edu>
2024-08-23 15:57:46 -0400

    Reading 08 Update

--- readings/lec07/setup.sh
+++
@@ -1,5 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/readings/lec07/square_lib.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/readings/lec07/square_lib.h
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/readings/lec07/square.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/readings/lec07/Makefile
-rm -rf setup.sh
---
+++ readings/lec08/setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/readings/lec08/square_lib.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/readings/lec08/square_lib.h
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/readings/lec08/square.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/readings/lec08/Makefile
+rm -rf setup.sh
