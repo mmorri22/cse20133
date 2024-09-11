9973b3ffc94b529c36b91fa2ca4aa2f752cbe5a5
Matthew Morrison <mmorri22@nd.edu>
2024-09-11 08:44:31 -0400

    Homework 03 Push

---
+++ homeworks/homework03/setup.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework03/Prob1.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework03/Prob2.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework03/Prob3.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework03/Prob4.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework03/Prob5.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/homeworks/homework03/Makefile
+rm setup.sh
