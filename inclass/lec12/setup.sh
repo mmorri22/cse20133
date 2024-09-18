3a87ee62db8d9bc85eb28932efbfca8cda74ff87
Matthew Morrison <mmorri22@nd.edu>
2024-09-18 11:46:49 -0400

    Lecture 15 Reading and In Class Update

--- inclass/lec12/setup.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec12/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec12/trace.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec12/trace_ptr.c
-rm setup.sh
---
+++ inclass/lec15/setup.sh
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/trace.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/trace_ptr.c
+rm setup.sh
