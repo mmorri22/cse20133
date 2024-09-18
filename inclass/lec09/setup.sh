862f93943dc404cf7c3c668c70a63b9b26814936
Matthew Morrison <mmorri22@nd.edu>
2024-09-18 11:00:11 -0400

    Lecture 10 Update

--- inclass/lec09/setup.sh
+++
@@ -1,6 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/fscanf.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/fscanf_pbr.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/ptr_trace.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec09/ptr_trace_pbr.c
-rm setup.sh
---
+++ inclass/lec10/setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/fscanf.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/fscanf_pbr.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/ptr_trace.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec10/ptr_trace_pbr.c
+rm setup.sh
