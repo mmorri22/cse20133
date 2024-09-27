fe7678a4fa7b036d00b9455dd1443bb249511f24
Matthew Morrison <mmorri22@nd.edu>
2024-09-27 10:34:21 -0400

    Lecture 11 Initial Push

--- inclass-sols/lec11/static_basic.c
+++ inclass-sols/lec11/static_basic.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 
 /* Put the compiler directive here */
+#define ARRAY_LEN 6
 
 /* When we copy to static_basic_prb.c, put the function declaration */
 
 int main(){
 
   /* Allocate the static integer values */
+  int int_array[] = {12, 16, -22, 19, 5, 11};
 
+  char char_array[] = "Go Irish!";
 
   /* Iterate through the array and print the values */
+  fprintf( stdout, "%p %p\n", int_array, char_array );
 
+  int iter = 0;
+
+  fprintf( stdout, "%p %d\n", &iter, iter );
+
+  for(iter = 0; iter < ARRAY_LEN; iter++){
+
+    fprintf( stdout, "%d %p %d\n", iter, &int_array[iter], int_array[iter]);
+  }
+
+  for( iter = 0; iter < 9; iter++){
+    fprintf( stdout, "%d %p %c\n", iter, &char_array[iter], char_array[iter]);
+  }
 
   return 0;
 }
--- inclass-sols/lec11/static_pbr.c
+++ inclass-sols/lec11/static_pbr.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 
 /* Put the compiler directive here */
+#define ARRAY_LEN 6
 
 /* When we copy to static_basic_prb.c, put the function declaration */
+
+void print_arrays( int int_array[], char char_array[], long unsigned int char_array_len );
 
 int main(){
 
   /* Allocate the static integer values */
-  int int_array[] = {22, 17, -15, 8, 2, 15};
-  long unsigned int array_len = 6;
+  int int_array[] = {12, 16, -22, 19, 5, 11};
+
+  char char_array[] = "Go Irish!";
+
+  long unsigned int char_array_len = 9;
 
   /* Iterate through the array and print the values */
-  long unsigned int iter;
-  for( iter = 0; iter < array_len; ++iter ){
+  fprintf( stdout, "%p %p %p %p\n", int_array, &int_array, char_array, &char_array );
 
-    fprintf( stdout, "int_array[%lu] = %d at %p\n", iter, int_array[iter], &int_array[iter] );
+  fprintf( stdout, "%p %lu\n", &char_array_len, char_array_len);
 
-  }
+  print_arrays( int_array, char_array, char_array_len );
 
   return 0;
 }
 
 /* When we copy to static_basic_prb.c, put the function definition */
+void print_arrays( int int_array[], char char_array[], long unsigned int char_array_len ){
+
+  fprintf( stdout, "%p %p %p %p\n", int_array, &int_array, char_array, &char_array );
+
+  int iter = 0;
+
+  fprintf( stdout, "%p %d\n", &iter, iter );
+
+  for(iter = 0; iter < ARRAY_LEN; iter++){
+
+    fprintf( stdout, "%d %p %d\n", iter, &int_array[iter], int_array[iter]);
+  }
+
+  for( iter = 0; iter < (int)char_array_len; iter++){
+    fprintf( stdout, "%d %p %c\n", iter, &char_array[iter], char_array[iter]);
+  }
+
+}
