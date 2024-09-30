7147181654208cedcc026d2fe7812566e3fd7bef
Matthew Morrison <mmorri22@nd.edu>
2024-09-30 09:49:04 -0400

    fscanf code solution

--- inclass-sols/lec11/fscanf.c
+++ inclass-sols/lec11/fscanf.c
@@ -12,9 +12,10 @@ int main(){
   /* Step 1 - Write fscanf to read in the values */
   fprintf( stdout, "Enter an integer, float, double, and a string: ");
 
+  if( fscanf( stdin, "%d %f %lf %s", &first_int, &first_float, &first_double, first_string ) ){}
 
   /* Step 2 - Write fprintf to print all the values */
-
+  fprintf( stdout, "%d %f %lf %s\n", first_int, first_float, first_double, first_string );
 
   return 0;
 }
