c7e56ccc4d6d7fd3f90640ddb29d1c16799cb7e8
Matthew Morrison <mmorri22@nd.edu>
2024-10-04 08:24:37 -0400

    Hello World

--- inclass/lec15/setup.sh
+++ inclass/lec15/setup.sh
@@ -1,4 +1,6 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/trace.c
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/trace_ptr.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/fib.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/fib_memo.c
 rm setup.sh
