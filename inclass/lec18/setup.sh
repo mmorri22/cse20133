111bb6c5904aeebb2a94cb1d52a1496602be0235
Matthew Morrison <mmorri22@nd.edu>
2024-10-09 11:58:19 -0400

    Lecture 18 Initial Update

--- inclass/lec18/setup.sh
+++ inclass/lec18/setup.sh
@@ -1,4 +1,6 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/double.c
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/stat_dyn_func.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/stat_dyn_addr.c
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/dyn_array_time.c
 rm setup.sh
