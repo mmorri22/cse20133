863ff2aff3b68a2304532a703ec4474eb8e9fa33
Matthew Morrison <mmorri22@nd.edu>
2024-10-13 13:21:14 -0400

    Lecture 19 In Class Fix

---
+++ inclass/lec19/setup.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/better_malloc.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/better_malloc_li.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/better_malloc_float.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/better_malloc_double.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/argc_argv.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/argc_argv_mod.c
+rm setup.sh
