e92819a9dc999ab4b0cb80d980bfac7efd2d4504
Matthew Morrison <mmorri22@nd.edu>
2024-10-13 14:09:55 -0400

    Lecture 22 In Class

--- inclass/lec14_bu/setup.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec14/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec14/stat_dyn_addr.c
-rm setup.sh
--- inclass/lec15_bu/setup.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/stat_dyn_func.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec15/dyn_array_time.c
-rm setup.sh
