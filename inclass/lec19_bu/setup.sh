ee4531f87820f5ed386cec56271e54baf65ee785
Matthew Morrison <mmorri22@nd.edu>
2024-10-13 13:52:31 -0400

    Lecture 21 In Class

--- inclass/lec19_bu/setup.sh
+++
@@ -1,5 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/lec19.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/lec19_sol.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec19/library.txt
-rm setup.sh
---
+++ inclass/lec21/setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/lec21.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/lec21_sol.c
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/library.txt
+rm setup.sh
