23b87ee2d58c1aa0faa20ad6c0a3ce2927cb1971
Matthew Morrison <mmorri22@nd.edu>
2024-10-13 13:48:34 -0400

    Lecture 20 In Class

--- inclass/lec18_bu/setup.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/structs.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec18/mem_map.c
-rm setup.sh
