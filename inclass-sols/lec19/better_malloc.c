2ce925b73a0af01c4b0f0afff7c42705c1325f30
Matthew Morrison <mmorri22@nd.edu>
2024-10-14 10:17:08 -0400

    Lecture 19 In Class Update

--- inclass-sols/lec19/better_malloc.c
+++ inclass-sols/lec19/better_malloc.c
@@ -4,11 +4,13 @@
 /* function prototypes */
 
 /* Step 1a - Allocate the memory function declaration */
+void alloc_memory( long unsigned int num_elements, int** dyn_int_array );
 
 /* Step 2a - Print the memory function declaration */
+void print_memory( long unsigned int num_elements, int* dyn_arr_ptr );
 
 /* Step 3a - Free the Memory function declaration */
-
+void free_memory( int** dyn_int_array );
 
 int main()
 {
@@ -21,7 +23,9 @@ int main()
 
 
    fprintf( stdout, "Enter number of elements in integer values (greater than 0): ") ;
-   fscanf( stdin, "%lu", &num_elements) ;
+   if( fscanf( stdin, "%lu", &num_elements) == -1 ){
+      return EXIT_FAILURE;
+   }
 
    /* Check for memory leak */
    if (num_elements == 0){
@@ -30,6 +34,9 @@ int main()
    }
 
    /* 1c - Call the memory allocation function in main */
+   alloc_memory( num_elements, &int_dyn_arr );
+
+   fprintf( stdout, "Main - %p at %p\n", int_dyn_arr, &int_dyn_arr );
 
 
    /* Lets use the memory as an arrray */
@@ -43,19 +50,37 @@ int main()
    }
 
    /* 2c - Print the memory addresses and data */
-
+   print_memory( num_elements, int_dyn_arr );
 
    /* 3c - Free the memory */
-
+   free_memory( &int_dyn_arr );
 
    return 0;
 }
 
 
 /* Step 1b - Allocate the memory function definition */
+void alloc_memory( long unsigned int num_elements, int** dyn_int_array ){
 
+   fprintf( stdout, "before - %p %p %p\n", *dyn_int_array, dyn_int_array, &dyn_int_array );
+
+   *dyn_int_array = (int *)malloc( num_elements * sizeof(int) );
+
+   fprintf( stdout, "after - %p %p %p\n", *dyn_int_array, dyn_int_array, &dyn_int_array );
+
+}
 
 /* Step 2b - Print the memory function definition */
+void print_memory( long unsigned int num_elements, int* dyn_arr_ptr ){
 
+   long unsigned int iter;
+   for( iter = 0; iter < num_elements; ++iter ){
+
+      fprintf( stdout, "%d at %p\n", dyn_arr_ptr[iter], &dyn_arr_ptr[iter]);
+   }
+}
 
 /* Step 3b - Free the Memory function definition */
+void free_memory( int** dyn_int_array ){
+   free( *dyn_int_array );
+}
