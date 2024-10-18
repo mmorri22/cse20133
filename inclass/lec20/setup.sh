2dba1667041cd6d00c880a65f145fdb98f248c04
Matthew Morrison <mmorri22@nd.edu>
2024-10-18 07:42:23 -0400

    Update Lecture 22 Reading

--- inclass/lec20/setup.sh
+++
@@ -1,4 +0,0 @@
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec20/Makefile
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec20/structs.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec20/mem_map.c
-rm setup.sh
