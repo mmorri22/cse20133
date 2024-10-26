df9a920ba013a6695d95c198e5a0d714222ccd3d
Matthew Morrison <mmorri22@nd.edu>
2024-10-26 09:20:08 -0400

    Lecture 22 Update

--- inclass/lec21/setup.sh
+++ inclass/lec21/setup.sh
@@ -1,6 +1,4 @@
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/Makefile
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/structs.c
 wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/mem_map.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/struct_align.c
-wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec21/struct_align_fix.c
 rm setup.sh
