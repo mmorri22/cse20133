f48e8dab309afe8c805412eab3fa4a5541d5592a
Matthew Morrison <mmorri22@nd.edu>
2024-11-11 08:32:36 -0500

    Lecture 28 Initial Update

---
+++ inclass/lec28/setup.sh
@@ -0,0 +1,7 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec28/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec28/call_by.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec28/call_sol.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec28/inputs.txt
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec28/file_io.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec28/file_io_sol.cpp
+rm setup.sh
