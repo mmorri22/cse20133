dc3f005f0de2c22cb605f912e0f2b821c97a6372
Matthew Morrison <mmorri22@nd.edu>
2024-11-11 08:39:36 -0500

    Lecture 28 Update

---
+++ inclass/lec28/setup_bu.sh
@@ -0,0 +1,8 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/vector.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/count_sort.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/count_sort_sol.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/hanoi_stl.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/two_sums.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec27/sort_comp.cpp
+rm setup.sh
