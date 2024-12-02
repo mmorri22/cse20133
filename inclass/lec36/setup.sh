f719d569885fe83b6ec0e1be58de45ab946abe99
Matthew Morrison <mmorri22@nd.edu>
2024-12-02 06:39:09 -0500

    Matplot++ In-Class Update

---
+++ inclass/lec36/setup.sh
@@ -0,0 +1,10 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/minimal.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/3d_plot.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/scatter.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/scatter_sol.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/sin_cos.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/sin_cos_sol.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/electric.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec36/electric_sol.cpp
+rm setup.sh
---
+++ inclass/lec37/setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec37/Makefile
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec37/sin_cos.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec37/sin_cos_sol.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec37/electric.cpp
+wget https://raw.githubusercontent.com/mmorri22/cse20133/main/inclass/lec37/electric_sol.cpp
+rm setup.sh
