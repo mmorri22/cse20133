6ca8c63daf3988afc901b6719a67602d5b8ac932
Matthew Morrison <mmorri22@nd.edu>
2026-09-11 09:19:11 -0400

    Lecture 08 Updates

---
+++ inclass/lec08/func_no_compile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int add_func( int val_1, int val_2 ){
+  return val_1 + sub_func(val_2, val_1);
+}
+
+int sub_func( int val_1, int val_2 ){
+  return val_1 - val_2;
+}
+
+int main(){
+
+  int first_num = 10;
+  int second_num = 20;
+
+  int result_val = add_func( first_num, second_num );
+
+  fprintf( stdout, "%d\n", result_val );
+
+  return 0;
+}
